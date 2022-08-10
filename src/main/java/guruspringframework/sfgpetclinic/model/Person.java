0b08f623c39d2793035ce9300d465e3e0e79272d
hristokaraperev <hristokaraperev@gmail.com>
2022-08-10 09:13:01 +0300

    established basic model(POJOs). Closes #2.

---
+++ src/main/java/guruspringframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,26 @@
+package guruspringframework.sfgpetclinic.model;
+
+public class Person {
+
+    // == fields ==
+    private String firstName;
+    private String lastName;
+
+
+    // == public methods ==
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/main/java/guruspringframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,16 @@
+package guruspringframework.sfgpetclinic.model;
+
+public class PetType {
+
+    // == fields ==
+    private String name;
+
+    // == public methods ==
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
