63e2d6940e9ae152dcf7a14ccebaaa53f5198798
hristokaraperev <hristokaraperev@gmail.com>
2022-08-12 15:58:01 +0300

    updated owner with jpa entity mappings. closes #36

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,8 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
 
     // == fields ==
+    @Column(name = "name")
     private String name;
 
     // == public methods ==
